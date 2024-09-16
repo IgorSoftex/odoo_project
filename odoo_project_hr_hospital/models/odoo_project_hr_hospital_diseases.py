b69be2ecb5a47a73573bf0ec11cd5e7238044b9a
IgorSoftex <igor.softex@ukr.net>
2024-09-16 18:34:19 +0300

    Commit_4 16.09.2024 18:34

---
+++ odoo_project_hr_hospital/models/odoo_project_hr_hospital_diseases.py
@@ -0,0 +1,12 @@
+import logging
+from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+class HRHospitalDiseases(models.Model):
+    _name = 'odoo.project.hr.hospital.diseases'
+    _description = 'Diseases'
+
+    name = fields.Char()
+    active = fields.Boolean(default=True)
+    description = fields.Text()
