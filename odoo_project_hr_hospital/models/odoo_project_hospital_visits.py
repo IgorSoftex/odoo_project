fac3e7f318c4059ceb01a44c9c2a1846558b32b9
IgorSoftex <igor.softex@ukr.net>
2024-09-17 17:39:45 +0300

    Commit_5 16.09.2024 19:44

--- odoo_project_hr_hospital/models/odoo_project_hospital_visits.py
+++ odoo_project_hr_hospital/models/odoo_project_hospital_visits.py
@@ -7,9 +7,18 @@ class HRHospitalVisits(models.Model):
     _name = 'odoo.project.hospital.visits'
     _description = 'Patient visits'
 
-    visit_date = fields.Datetime(string='Visit date', required=True)
-    active = fields.Boolean(default=True)
+    visit_date = fields.Datetime(
+        string='Visit date',
+        required=True)
+    active = fields.Boolean(
+        default=True)
     description = fields.Text()
-    patient_id = fields.Many2one(comodel_name='odoo.project.hospital.patients', string='Patient')
-    doctor_id = fields.Many2one(comodel_name='odoo.project.hospital.doctors', string='Doctor')
-    disease_ids = fields.Many2many(comodel_name='odoo.project.hospital.diseases', string='Diseases')
+    patient_id = fields.Many2one(
+        comodel_name='odoo.project.hospital.patients',
+        string='Patient')
+    doctor_id = fields.Many2one(
+        comodel_name='odoo.project.hospital.doctors',
+        string='Doctor')
+    disease_ids = fields.Many2many(
+        comodel_name='odoo.project.hospital.diseases',
+        string='Diseases')
