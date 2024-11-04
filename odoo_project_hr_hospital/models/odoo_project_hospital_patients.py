e8abff479f4a872a2c6f36ce00bc4dd8be734831
IgorSoftex <igor.softex@ukr.net>
2024-11-04 19:37:41 +0200

    Commit_56 04.11.2024 19:37

--- odoo_project_hr_hospital/models/odoo_project_hospital_patients.py
+++ odoo_project_hr_hospital/models/odoo_project_hospital_patients.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import models, fields
 
 
 class HRHospitalPatients(models.Model):
@@ -6,3 +6,12 @@ class HRHospitalPatients(models.Model):
     _description = 'Patients'
     _inherit = "odoo.project.hospital.person"
     _rec_name = 'full_name'
+
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Partner",
+    )
+
+    def patient_test_action(self):
+        print('partner_id:', self.partner_id.name, self.partner_id.id)
+        print('user.partner_id:', self.env.user.partner_id.name, self.env.user.partner_id.id)
